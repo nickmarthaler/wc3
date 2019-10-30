5703586e3038b110abe8f01fe83b03f0149185c8
Nick Marthaler <nicholas.marthaler@gmail.com>
2019-10-29 20:28:02 -0500

    first commit

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+
+
+public class MyClass {
+    public static void main(String[] args) {
+        if (20 > 11) {
+            System.out.println("20 is greater than 10" + args[0]);
+        }
+    }
+}
