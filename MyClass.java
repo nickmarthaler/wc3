d5f9f419417513192adc4876a7137afb5e333f47
Nick Marthaler <nicholas.marthaler@gmail.com>
2019-10-31 20:14:39 -0500

    Finished 'while loop' Section

--- MyClass.java
+++ MyClass.java
@@ -2,28 +2,11 @@
 
 public class MyClass {
     public static void main(String[] args) {
-    int day = 7;
-    switch (day) {
-        case 1:
-            System.out.println("Monday");
-            break;
-        case 2:
-            System.out.println("Tuesday");
-            break;
-        case 3:
-            System.out.println("Wednesday");
-            break;
-        case 4:
-            System.out.println("Thursday");
-            break;
-        case 5:
-            System.out.println("Friday");
-            break;
-        case 6:
-            System.out.println("Saturday");
-        case 7:
-            System.out.println("Sunday");
-            break;
+    int i = 0;
+    do {
+        System.out.println(i);
+        i++;
     }
+    while (i < 5);
     }
 }
