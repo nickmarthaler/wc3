7086711f58c1686ce2f12e0c4c5fbfb67e88d6ba
Nick Marthaler <nicholas.marthaler@gmail.com>
2019-10-31 19:51:15 -0500

    Finished 'If' Section

--- MyClass.java
+++ MyClass.java
@@ -2,8 +2,28 @@
 
 public class MyClass {
     public static void main(String[] args) {
-        if (20 > 11) {
-            System.out.println("20 is greater than 10" + args[0]);
-        }
+    int day = 7;
+    switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+        case 7:
+            System.out.println("Sunday");
+            break;
+    }
     }
 }
