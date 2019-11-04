3bfaac778bec14b70c36d92638206b3d1f79c4d4
Nick Marthaler <nicholas.marthaler@gmail.com>
2019-11-03 19:44:00 -0600

    Adding Method example

---
+++ Method.java
@@ -0,0 +1,14 @@
+public class Method {
+    static void checkAge(int age) {
+
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkAge(20);
+    }
+}
