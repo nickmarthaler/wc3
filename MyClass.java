f0e62ff2dde81b2c4ae33c36731304b7fd57d8e7
Nick Marthaler <nicholas.marthaler@gmail.com>
2019-11-03 19:10:39 -0600

    End of Array Section

--- MyClass.java
+++ MyClass.java
@@ -1,12 +1,11 @@
 
-
 public class MyClass {
     public static void main(String[] args) {
-    int i = 0;
-    do {
-        System.out.println(i);
-        i++;
-    }
-    while (i < 5);
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        for (int i = 0; i < myNumbers.length; ++i) {
+            for(int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
     }
 }
