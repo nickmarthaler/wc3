9aef2b77535327aef43e5953cd339d9e8650be13
Nick Marthaler <nicholas.marthaler@gmail.com>
2019-11-04 20:13:55 -0600

    Midway through Java Class Methods

---
+++ Object.java
@@ -0,0 +1,19 @@
+public class Object {
+    //Static method
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+    //Public method
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
+    }
+    //Main method
+    public static void main(String[] args) {
+        myStaticMethod(); //Call the static method
+        //myPublicMethod(); This would compile an error.
+
+        Object myObj = new Object(); //Create an object of Object
+        myObj.myPublicMethod(); //Call the public method on the object
+    }
+
+}
